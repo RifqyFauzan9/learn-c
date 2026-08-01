e58900685dd1410fa62c96cd3964040e4dd3f9f5
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-08-01 18:57:39 +0700

    memuat file initvar.c dan menuliskan script c didalamnya

---
+++ initvar.c
@@ -0,0 +1,10 @@
+/* File: halo.c */
+/* Penulis: Rifqy Fauzan, email: rifqyfauzann1@gmail.com */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+
+#include<stdio.h>
+int main() {
+    int i = 5;
+    printf("Ini nilai i : %d\n", i);
+    return 0;
+}
