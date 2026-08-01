bfb17a99b89e7f2ab601482c8e22980a56638015
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-08-01 18:41:07 +0700

    membuat file halo.c dan menuliskan script c didalamnya

---
+++ halo.c
@@ -0,0 +1,11 @@
+/* File: halo.c */
+/* Penulis: Rifqy Fauzan, email: rifqyfauzann1@gmail.com */
+/* Program menuliskan 'halo dunia' dan 'end-of-line' (ganti baris) */
+#include<stdio.h>
+int
+main(void)
+{ /* Badan program */
+    printf("Halo, Dunia!\n ");
+    printf("Halo, Dunia!\n ");
+    printf("Halo, Dunia!\n ");
+}
