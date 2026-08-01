fc9a5a5a1114f328947c15ee32e72785d9327a20
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-08-01 18:34:15 +0700

    reset projek

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char nama[50];
-    printf("Masukkan nama kamu: ");
-    scanf("%s", nama);
-    printf("Halo %s, setup C di MacBook kamu berhasil!\n", nama);
-    return 0;
-}
