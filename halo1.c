0e929997293493d842e92102854ed916f197072d
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-08-01 18:50:21 +0700

    membuat filehalo1.c dan menuliskan script c didalamnya

---
+++ halo1.c
@@ -0,0 +1,13 @@
+/* File: halo.c */
+/* Penulis: Rifqy Fauzan, email: rifqyfauzann1@gmail.com */
+/* Program menuliskan 'halo dunia' ke layar */
+#include<stdio.h>
+int
+main () 
+{
+    /* Kamus */
+
+    /* Algoritma */
+    printf("Halo dunia\n");
+    return 0;
+}
