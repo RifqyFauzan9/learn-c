977770a55cde1ee2879226e1fb726dbd6293d4da
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-08-01 18:28:06 +0700

    first commit learning c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char nama[50];
+    printf("Masukkan nama kamu: ");
+    scanf("%s", nama);
+    printf("Halo %s, setup C di MacBook kamu berhasil!\n", nama);
+    return 0;
+}
